1ba7375036a4506bd1b3e1534393614bb2345fe8
Mika Rantanen <mika.p.rantanen@helsinki.fi>
2016-06-23 14:32:48 +0300

    deleted calc_htends

--- src/ozo.f90
+++ src/ozo.f90
@@ -7,11 +7,10 @@ program zo
   character :: mode
   real :: alfa, toler
   integer :: time_1, time_n
-  logical :: calc_htends,calc_omegas
+  logical :: calc_omegas
   type ( wrf_file ) :: wrfin_file, omegafile
 
-  namelist/PARAM/infile,outfile,alfa,toler,time_1,time_n,mode,calc_htends,&
-       calc_omegas
+  namelist/PARAM/infile,outfile,alfa,toler,time_1,time_n,mode,calc_omegas
   read(*,nml=PARAM)
 
   wrfin_file = open_wrf_file ( infile )
@@ -22,7 +21,7 @@ program zo
   end if
 
   call time_step_loop ( wrfin_file, omegafile, time_1, time_n, alfa, toler, &
-                        mode, calc_htends, calc_omegas)
+                        mode, calc_omegas)
   call close_wrf_file ( wrfin_file )
   call close_wrf_file ( omegafile )
 
